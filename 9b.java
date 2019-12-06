187fdc21dcce17a38259c776df4dca668fe56cfa
123303 <58052306+123303@users.noreply.github.com>
2019-12-06 13:24:08 +0000

    Update 9b.java

--- 9b.java
+++ 9b.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet; 
 import javax.servlet.http.*;
-@WebServlet("/Servlet11b")
+@WebServlet("/Servlet9b")
 public class 9b extends HttpServlet {
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 response.setContentType("text/html");
